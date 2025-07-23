2ddcd8ced57122fb4e06a712fb3c966844698860
Jiri Till <jirik.till@gmail.com>
2025-07-23 11:03:35 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-2xl font-bold">SuppPlan</Link>
+        <div>
+          <Link to="/planner" className="mx-2 hover:underline">Planner</Link>
+          <Link to="/library" className="mx-2 hover:underline">Library</Link>
+          <Link to="/blog" className="mx-2 hover:underline">Blog</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
