1f32e88e411c32f86947c92a7ac43b4e00770319
Jiri Till <jirik.till@gmail.com>
2025-07-23 11:00:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import PlannerPage from './pages/PlannerPage';
+import BlogPage from './pages/BlogPage';
+import LibraryPage from './pages/LibraryPage';
+import SupplementDetailPage from './pages/SupplementDetailPage';
+import TermsOfServicePage from './pages/TermsOfServicePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { AppProvider } from './contexts/AppContext';
 
+function App() {
+  return (
+    <AppProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto p-4">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/planner" element={<PlannerPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/library" element={<LibraryPage />} />
+              <Route path="/library/:slug" element={<SupplementDetailPage />} />
+              <Route path="/terms" element={<TermsOfServicePage />} />
+              {/* Add more routes as you build out pages */}
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AppProvider>
+  );
+}
+
+export default App;
