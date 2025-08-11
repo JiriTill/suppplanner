d94eb5465eece34bf77ad92d98e143d77a4be10a
Jiri Till <jirik.till@gmail.com>
2025-08-11 14:42:06 +0200

    Update PlannerPage.js

--- src/pages/PlannerPage.js
+++ src/pages/PlannerPage.js
@@ -1,4 +1,6 @@
+// src/pages/PlannerPage.js
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // <-- This line was missing
 import { generateSupplementPlan } from '../services/api';
 import { useFirebase } from '../contexts/FirebaseContext';
 
